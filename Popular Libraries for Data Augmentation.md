9ed9d2e29f9be342fa86cf9c4b8d6c3bd7e70ff7
satyazmx <thakursatya6@gmail.com>
2023-02-13 10:38:38 +0530

    add audio tranformation example

---
+++ Popular Libraries for Data Augmentation.md
@@ -0,0 +1,15 @@
+Audio Data Augmentation Libraries in Python
+<img width="30px" src="https://media.tenor.com/images/3b388fe03da271d2674faf85eb7c3fcd/tenor.gif" />
+
+<br/>
+
+### Librosa
+![Screenshot](snips/librosa.png)
+### Audiomentations
+![Screenshot](snips/audiomentations.png)
+### Torch Audiomentations
+![Screenshot](snips/torch_audiomentations.png)
+### Torch Audio
+![Screenshot](snips/torch_audio.png)
+### Which Library Should I use ??
+![Screenshot](snips/which.png)
---
+++ train.py
@@ -0,0 +1,106 @@
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+# 1- download dataset
+# 2- create data loader
+# 3- build model
+# 4- train
+# 5- save trained model
+
+BATCH_SIZE = 128
+EPOCHS = 10
+LEARNING_RATE = 0.001
+
+class FeedForwardNet(nn.Module):
+    def __init__(self) :
+        super().__init__()
+        self.flatten = nn.Flatten()
+        self.dense_layers = nn.Sequential(
+            nn.Linear(28*28,256),
+            nn.ReLU(),
+            nn.Linear(256,10)
+        )
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, input_data):
+        x = self.flatten(input_data)
+        logits = self.dense_layers(x)
+        predictions = self.softmax(logits)
+        return predictions
+
+def train_single_epoch(model, data_loader, loss_fn, optimiser, device):
+    for input, target in data_loader:
+        input, target = input.to(device), target.to(device)
+
+        # calculate loss
+        prediction = model(input)
+        loss = loss_fn(prediction, target)
+
+        # backpropagate error and update weights
+        optimiser.zero_grad()
+        loss.backward()
+        optimiser.step()
+    
+    print(f"loss: {loss.item()}")
+
+
+def train(model, data_loader, loss_fn, optimiser, device,epochs):
+    for i in range(epochs):
+        print(f"Epoch {i+1}")
+        train_single_epoch(model, data_loader, loss_fn, optimiser, device)
+        print("---------------------------")
+    print("Finished training")
+
+
+
+def download_mnist_data():
+    train_data = datasets.MNIST(
+        root= 'data',
+        download=True,
+        train=True,
+        transform=ToTensor()
+    )
+    validation_data = datasets.MNIST(
+        root= 'data',
+        download=True,
+        train=False,
+        transform=ToTensor()
+    )
+    return train_data, validation_data
+
+
+
+def create_data_loader(train_data):
+    train_dataloader = DataLoader(train_data, batch_size=BATCH_SIZE)
+    return train_dataloader
+
+
+if __name__ == "__main__":
+
+    # download data and create data loader
+    train_data, _ = download_mnist_data()
+    train_dataloader = create_data_loader(train_data)
+
+    # construct model and assign it to device
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    print(f"Using {device}")
+    feed_forward_net = FeedForwardNet().to(device)
+    print(feed_forward_net)
+
+    # initialise loss funtion + optimiser
+    loss_fn = nn.CrossEntropyLoss()
+    optimiser = torch.optim.Adam(feed_forward_net.parameters(),
+                                 lr=LEARNING_RATE)
+
+    # train model
+    train(feed_forward_net, train_dataloader, loss_fn, optimiser, device, EPOCHS)
+
+    # save model
+    torch.save(feed_forward_net.state_dict(), "feedforwardnet.pth")
+    print("Trained feed forward net saved at feedforwardnet.pth")
